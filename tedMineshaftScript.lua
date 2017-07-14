66daf08e1f2a141ddd03a7733846f15167731622
popcheese9 <popcheese9@gmail.com>
2017-07-14 09:41:10 -0400

    Updated: tedMineshaftScript.lua

--- tedMineshaftScript.lua
+++ tedMineshaftScript.lua
@@ -3,6 +3,7 @@ args = {...}
 digLength = args[1]
 digWidth = args[2]
 digDepth = args[3]
+preDist = args[4]
 
 function turtleRight()
 	turtle.turnRight()
@@ -30,30 +31,56 @@ function turtleTurnAround()
 	turtle.turnLeft()
 end
 
-for i=1,digDepth/2 do
-turtle.digDown()
-turtle.down()
-	for x=1,(digWidth/2)-1 do
-		turtleRow()
-		turtleRight()
-		turtleRow()
-		turtleLeft()
-	end
-	turtleRow()
-	turtleRight()
-	turtleRow()
-	turtleTurnAround()
+if preDist == 0 then
+	for i=1,digDepth/2 do
+		for x=1,(i-1)*2 do
+			turtle.down()
+		end
 	turtle.digDown()
 	turtle.down()
-	for x=1, (digWidth/2)-1 do
+		for x=1,(digWidth/2)-1 do
+			turtleRow()
+			turtleRight()
+			turtleRow()
+			turtleLeft()
+		end
+		turtleRow()
+		turtleRight()
+		turtleRow()
+		turtleTurnAround()
+		turtle.digDown()
+		turtle.down()
+		for x=1, (digWidth/2)-1 do
+			turtleRow()
+			turtleLeft()
+			turtleRow()
+			turtleRight()
+		end
 		turtleRow()
 		turtleLeft()
 		turtleRow()
-		turtleRight()
+		turtleTurnAround()
+		print(i*2 .. " finished out of " .. digDepth)
+		-- turtle.up()
+		turtle.up()
+		for x=1,i*2 do
+			turtle.up()
+		end
+		turtle.turnLeft()
+		-- turtle.forward()
+		for x=1,16 do
+		  turtle.select(x)
+		  turtle.drop()
+		end
+		turtle.turnRight()
+		turtle.turnRight()
+		-- turtle.forward()
+		turtle.down()
+		-- turtle.down()
+		turtle.turnRight()
+		turtle.suck(64)
+		turtle.refuel()
+		turtleTurnAround()
 	end
-	turtleRow()
-	turtleLeft()
-	turtleRow()
-	turtleTurnAround()
-	print(i .. " finished out of " .. digDepth)
-end
+else
+	
